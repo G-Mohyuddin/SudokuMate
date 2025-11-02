08ee92cd373e937179fe47e16e519df0973ee2d7
G-Mohyuddin <ghulammohyuddinibji@gmail.com>
2025-11-02 16:47:34 +0530

    Fix formatting and structure definitions in structs.h

--- structs.h
+++ structs.h
@@ -8,18 +8,18 @@
 #define GRID_SIZE 9
 
 typedef struct Hint {
-	int triggerCellCount; // when remaining empty cells equals this, show this hint
+	int triggerCellCount;
 	char message[256];
-	int hintRow; // optional: row for a suggested move (-1 if N/A)
-	int hintCol; // optional: col for a suggested move (-1 if N/A)
-	int hintValue; // optional: value for a suggested move (0 if N/A)
+	int hintRow; 
+	int hintCol; 
+	int hintValue;
 } Hint;
 
 typedef struct Puzzle {
 	char name[64];
 	char difficulty[32];
-	int grid[GRID_SIZE][GRID_SIZE]; // initial givens (0 means empty)
-	int solution[GRID_SIZE][GRID_SIZE]; // optional: filled by loader/solver
+	int grid[GRID_SIZE][GRID_SIZE]; 
+	int solution[GRID_SIZE][GRID_SIZE]; 
 	Hint hints[16];
 	size_t numHints;
 } Puzzle;
@@ -33,21 +33,22 @@ typedef struct Move {
 
 typedef struct GameState {
 	int currentGrid[GRID_SIZE][GRID_SIZE];
-	bool isFixed[GRID_SIZE][GRID_SIZE]; // true if original given, not editable
+	bool isFixed[GRID_SIZE][GRID_SIZE]; 
 	char activeDifficulty[32];
 	char activePuzzleName[64];
 
 	Move moves[128];
 	size_t moveCount;
-	size_t redoCount; // number of redoable moves stored after undos
+	size_t redoCount; 
 
-	time_t timerStart; // 0 when not started
-	unsigned int elapsedSeconds; // accumulated when paused/saved
+	time_t timerStart;
+	unsigned int elapsedSeconds; 
 
-	// index of next hint to trigger from the loaded puzzle
+	
 	size_t nextHintIndex;
 } GameState;
 
-#endif // STRUCTS_H
+#endif 
+
 
 
