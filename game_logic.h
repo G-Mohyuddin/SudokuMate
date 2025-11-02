a206cb6e60291430444b752e6558a375b66d6cd2
G-Mohyuddin <ghulammohyuddinibji@gmail.com>
2025-11-02 16:48:22 +0530

    Fix header guards and clean up game_logic.h

--- game_logic.h
+++ game_logic.h
@@ -6,12 +6,13 @@
 
 bool isMoveValid(const int grid[GRID_SIZE][GRID_SIZE], int row, int col, int value);
 
-// Fills poss[1..9] with 1 if value is possible at (row,col), else 0. Returns count.
+
 int computePossibilities(const int grid[GRID_SIZE][GRID_SIZE], int row, int col, int poss[10]);
 
 bool isBoardComplete(const int grid[GRID_SIZE][GRID_SIZE]);
 bool isBoardValid(const int grid[GRID_SIZE][GRID_SIZE]);
 
-#endif // GAME_LOGIC_H
+#endif
+
 
 
